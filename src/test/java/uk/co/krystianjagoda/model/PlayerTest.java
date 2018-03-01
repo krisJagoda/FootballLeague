142d4cd6ee79c9c4f7d0d15b74189f57938e0a44
Krystian Jagoda <krystian.jagoda@outlook.com>
2018-03-01 01:42:05 +0100

    Test player class

---
+++ src/test/java/uk/co/krystianjagoda/model/PlayerTest.java
@@ -0,0 +1,46 @@
+package uk.co.krystianjagoda.model;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class PlayerTest {
+
+    @Test
+    public void forAnEmptyStringAsPlayersFirstNameThrowAnIllegalArgumentException() throws Exception {
+
+        assertThatThrownBy(() -> new Player("","Doe"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("You must provide both the first and last name of the player.");
+    }
+
+    @Test
+    public void forAnEmptyStringAsPlayersLastNameThrowAnIllegalArgumentException() throws Exception {
+
+        assertThatThrownBy(() -> new Player("John", ""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("You must provide both the first and last name of the player.");
+    }
+
+    @Test
+    public void forANullValueGivenAsPLayersFirstNameThrowIllegalArgumentException() throws Exception {
+
+        assertThatThrownBy(() -> new Player(null, "Doe"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("You must provide both the first and last name of the player.");
+    }
+    @Test
+    public void forANullValueGivenAsPLayersLastNameThrowIllegalArgumentException() throws Exception {
+
+        assertThatThrownBy(() -> new Player("John", null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("You must provide both the first and last name of the player.");
+    }
+
+    @Test
+    public void forGivenFirstNameAndLastNameCreateANewInstanceOfPlayerClass() throws Exception {
+
+        new Player("John", "Doe");
+    }
+}
