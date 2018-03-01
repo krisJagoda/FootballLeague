ef8789f3c4a0950869525fd94e2e82d48f542c39
Krystian Jagoda <krystian.jagoda@outlook.com>
2018-03-02 00:37:32 +0100

    Modify team and teamtest classes

--- src/main/java/uk/co/krystianjagoda/model/Team.java
+++ src/main/java/uk/co/krystianjagoda/model/Team.java
@@ -1,29 +1,23 @@
 package uk.co.krystianjagoda.model;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Team {
     private final String name;
-    private final Set<Player> teamMembers;
+    private final Set<Player> teamMembers = new HashSet<>();
 
-    public Team(String name, Set<Player> teamMembers) throws IllegalArgumentException {
+    public Team(String name) throws IllegalArgumentException {
 
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException(String.format("You cannot provide either empty or %s team's name.", name));
         }
-
-        if (teamMembers == null) {
-            throw new IllegalArgumentException("Team cannot be null.");
-        }
-
         this.name = name;
-        this.teamMembers = new HashSet<>(teamMembers);
+
     }
 
     public void addNewTeamMember(Player player) {
 
-        if (player == null){
+        if (player == null) {
             throw new IllegalArgumentException("New players cannot be null.");
         }
         teamMembers.add(player);
@@ -38,27 +32,27 @@ public class Team {
     }
 
     @Override
+    public String toString() {
+        return "Team{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Team)) return false;
 
         Team team = (Team) o;
 
-        return getName().equals(team.getName())
-                && getTeamMembers().equals(team.getTeamMembers());
+        return (getName() != null ? getName().equals(team.getName()) : team.getName() == null)
+                && (getTeamMembers() != null ? getTeamMembers().equals(team.getTeamMembers()) : team.getTeamMembers() == null);
     }
 
     @Override
     public int hashCode() {
-        int result = getName().hashCode();
-        result = 31 * result + getTeamMembers().hashCode();
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getTeamMembers() != null ? getTeamMembers().hashCode() : 0);
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Team{" +
-                "name='" + name + '\'' +
-                '}';
     }
 }
--- src/test/java/uk/co/krystianjagoda/model/TeamTest.java
+++ src/test/java/uk/co/krystianjagoda/model/TeamTest.java
@@ -1,91 +1,54 @@
 package uk.co.krystianjagoda.model;
 
-import org.assertj.core.api.Assertions;
+
 import org.junit.Test;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TeamTest {
 
-    private Set<Player> players = new HashSet<>();
-    private String name;
-
     @Test
     public void forAnEmptyStringAsTeamsNameThrowAnIllegalArgumentException() throws Exception {
 
-        name = "";
-        players.add(new Player("John", "Doe"));
-
-        assertThatThrownBy(() -> new Team(name, players))
+        assertThatThrownBy(() -> new Team(""))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(String.format("You cannot provide either empty or %s team's name.", name));
+                .hasMessage(String.format("You cannot provide either empty or %s team's name.", ""));
     }
 
     @Test
     public void forANullValueProvidedAsTeamThrowIllegalArgumentException() throws Exception {
 
-        name = null;
-        players.add(new Player("John", "Doe"));
 
-        assertThatThrownBy(() -> new Team(name, players))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(String.format("You cannot provide either empty or %s team's name.", name));
+        assertThatThrownBy(() -> new Team(null))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 
-    @Test
-    public void forAEmptySetProvidedThrowIllegalArgumentException() throws Exception {
-
-        name = "Manchester United";
-        players = null;
-
-        assertThatThrownBy(() -> new Team(name, players))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Team cannot be null.");
-    }
 
     @Test
-    public void forNonEmptyNameAndNotNullSetOfPlayersCreateANewInstanceOfTheTeamClass() throws Exception {
+    public void forANonEmptyOrNotNullNameCreateNewTeam() throws Exception {
 
-        name = "Manchester United";
-        players.add(new Player("John", "Doe"));
-
-        new Team(name, players);
-    }
-
-    @Test
-    public void forANullPlayerThrowAnIllegalArgumentException() throws Exception {
-
-        name = "Manchester United";
-        players.add(new Player("John", "Doe"));
-
-        assertThatThrownBy(() -> new Team(name, players).addNewTeamMember(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("New players cannot be null.");
+        new Team("Manchester United");
     }
 
     @Test
     public void forTwoDifferentPlayersAddThemToTheTeam() throws Exception {
-
-        name = "Manchester United";
-        players.add(new Player("John", "Doe"));
-        Team team = new Team(name, players);
-
+        Team team = new Team("Manchester United");
         team.addNewTeamMember(new Player("Berry", "Walt"));
+        team.addNewTeamMember(new Player("John", "Doe"));
 
-
-        Assertions.assertThat(team.getTeamMembers()).contains(new Player("John", "Doe"), new Player("Berry", "Walt"));
+        assertThat(team.getTeamMembers()).contains(new Player("John", "Doe"), new Player("Berry", "Walt"));
 
     }
 
     @Test
     public void forAddingTheSamePlayerTwiceHeIsAssignedOnlyOnce() throws Exception {
-        name = "Manchester United";
-        players.add(new Player("John", "Doe"));
-        Team team = new Team(name, players);
+        Team team = new Team("Manchester United");
 
         team.addNewTeamMember(new Player("John", "Doe"));
+        team.addNewTeamMember(new Player("John", "Doe"));
 
-        Assertions.assertThat(team.getTeamMembers()).containsOnly(new Player("John", "Doe"));
+        assertThat(team.getTeamMembers()).containsOnly(new Player("John", "Doe"));
     }
 }
